cda2eaeed7a108de135ef6433dce637c828eac14
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2023-03-13 05:04:35 -0700

    Format data from xls to a dictionary

--- xlsdata.py
+++ xlsdata.py
@@ -1,37 +1,43 @@
 import xlrd
+book = xlrd.open_workbook('pagina.xls')
+sheets = book.nsheets
+information = {
+        'PRODUCTOS': [],
+        'DISPONIBILIDAD':{},
+        'INFORMACION':{}
+}
+
 def loadXLS():
-    book = xlrd.open_workbook('pagina.xls')
-    print (book.nsheets)
-    sheet = book.sheet_by_index(0)
-    row = 0
-    col = 0
-    cell = sheet.cell(row,col)
-    print (cell.value)
-    num_rows = sheet.nrows
-    num_col = sheet.ncols
-    print (num_rows)
-    print (num_col)
-    sheets = book.sheet_names()
-    cur_sheet = book.sheet_by_name(sheets[0])
-    print (cur_sheet)
+    for sheet in book:
+        num_rows = sheet.nrows
+        for row in range(1,num_rows):
+            if sheet.name == "PRODUCTOS":
+                information['PRODUCTOS'].append(format_product(sheet,row))
+            elif sheet.name == "DISPONIBILIDAD":
+                information['DISPONIBILIDAD'][int(float(str(sheet.cell(row, 0).value)))] = bool(int(str(sheet.cell(row, 1).value)[:1]))
+            elif sheet.name == 'INFORMACION':
+                information['INFORMACION'][str(sheet.cell(row, 0).value)] = str(sheet.cell(row, 1).value)
+    return information
 
-def getTitle():
-    book = xlrd.open_workbook('pagina.xls')    
-    sheet = book.sheet_by_index(0)
-    cell = sheet.cell(0,1)
-    return cell.value
+def format_product(sheet, row):
+    return {
+        'id': int(float(str(sheet.cell(row, 0).value))),
+        'nombre': str(sheet.cell(row, 1).value),
+        'precio': float(str(sheet.cell(row, 2).value)),
+        'cantidad': float(str(sheet.cell(row, 3).value)),
+        'categoria': str(sheet.cell(row, 4).value),
+        'imagen': str(sheet.cell(row, 5).value),
+    }
 
-def getAboutImg():
-    book = xlrd.open_workbook('pagina.xls')    
-    sheet = book.sheet_by_index(0)
-    cell = sheet.cell(1,1)
-    return cell.value
+def is_available(id):
+    return information["DISPONIBILIDAD"][id]
+                
+#esta funcion valida que la columna sea la de los ID y busca un id especifico
+def get_product_by_id(id):
+    for product in information["PRODUCTOS"]:
+        if product == id:
+            return product
 
-def getAboutText():
-    book = xlrd.open_workbook('pagina.xls')    
-    sheet = book.sheet_by_index(0)
-    cell = sheet.cell(2,1)
-    return cell.value
-
+#find_id_col(2,1)
 loadXLS()
-print("Loading data for web")
+#print("Loading data for web")
