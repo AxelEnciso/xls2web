a0bf2c83b2d9ceafb133e89011d3a598ca01932e
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2023-03-15 18:32:57 -0700

    cambiando disponibilidad como atributo de producto

--- app/utils/write_xls.py
+++ app/utils/write_xls.py
@@ -2,32 +2,37 @@ from app.utils.read_xls import *
 
 read_all_xls()
 wb = copy(book)
+sheet_information = wb.get_sheet(0)
 sheet_products = wb.get_sheet(1)
 sheet_available = wb.get_sheet(2)
 
+def save_wb():
+    wb.save('./app/data/pagina.xls')
+
+def next_id():
+    next_id = parseInt(information["INFORMACION"]["NEXT_ID"])
+    sheet_information.write(4, 1, next_id+1)
+    save_wb()
+    information["INFORMACION"]["NEXT_ID"] = next_id + 1
+    return next_id
+
 def create_product(product):
     next_row = len(information["PRODUCTOS"]) + 1
-    product["disponible"] = "disponible" in product
     write_product_xls(next_row, product)
     information["PRODUCTOS"].append(product)
-    information["DISPONIBILIDAD"][parseInt(product["id"])] = product["disponible"]
 
 def update_product(prev, product):
     index = get_product_idx(prev)
-    product["disponible"] = "disponible" in product
     write_product_xls(index+1, product)
     information["PRODUCTOS"][index] = product
-    information["DISPONIBILIDAD"][parseInt(product["id"])] = product["disponible"]
 
 def delete_product(product):
     index = get_product_idx(product)
-    product["disponible"] = "disponible" in product
     len_products = len(information["PRODUCTOS"])
-    write_product_xls(index + 1, get_product_by_idx(len_products - 1))
+    write_product_xls(index + 1, information["PRODUCTOS"][len_products - 1])
     delete_product_xls(len_products)
     information["PRODUCTOS"][index] = information["PRODUCTOS"][len_products - 1]
     del information["PRODUCTOS"][len_products - 1]
-    del information["DISPONIBILIDAD"][parseInt(product["id"])]
 
 def write_product_xls(index, product):
     sheet_products.write(index, 0, int(product["id"]))
@@ -38,7 +43,7 @@ def write_product_xls(index, product):
     sheet_products.write(index, 5, str(product["imagen"]))
     sheet_available.write(index, 0, int(product["id"]))
     sheet_available.write(index, 1, product["disponible"])
-    wb.save('./app/data/pagina.xls')
+    save_wb()
 
 def delete_product_xls(index):
     sheet_products.write(index, 0, "")
@@ -49,4 +54,4 @@ def delete_product_xls(index):
     sheet_products.write(index, 5, "")
     sheet_available.write(index, 0, "")
     sheet_available.write(index, 1, "")
-    wb.save('./app/data/pagina.xls')
+    save_wb()
